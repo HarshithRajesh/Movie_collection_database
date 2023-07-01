30a0a4e50da429ac62436db905469fc24cacb4b6
HarshithRajesh <harshithr2004@gmail.com>
2023-07-01 09:49:35 +0530

    secret key updated

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import os
 
 load_dotenv()
 #To connect to database
+SECRET_KEY = os.getenv("SECRET_KEY")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = SECRET_KEY
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-movie-collection.db"
 db = SQLAlchemy(app)
